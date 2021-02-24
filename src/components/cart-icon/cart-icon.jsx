a5d4cf7e5a26c083911d8918e4f47584adf1754b
Arzakan-Porchkhidze <arzakanporchkhidze17@gmail.com>
2021-02-24 23:50:39 +0400

    updating app with reselect

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { connect } from "react-redux";
 import { BsBag } from "react-icons/bs";
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import "./cart-icon.scss";
 
 
 
-const CartIcon = ({toggleCartHidden, red}) => {
+const CartIcon = ({toggleCartHidden, red, itemCount}) => {
   return ( 
     <div className='cart-icon' onClick = {toggleCartHidden} >
       <BsBag className={`shopping-icon ${red ? 'red' : ''}`} />
-      <span className='item-count' >0</span>
+      <span className='item-count' >{itemCount}</span>
     </div>
   );
 }
 
-const mapStateToProps = ({cart: {red}}) => ({
-  red
+const mapStateToProps = (state) => ({
+  red: state.cart.red,
+  itemCount: selectCartItemsCount(state)
 })
 
 const mapDispatchToProps = dispatch => ({
